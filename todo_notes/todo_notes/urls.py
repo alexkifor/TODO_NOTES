7aaad582c1a6011d8fa8fa19b6c77f3ba8688d9c
Aleksandr <8712060@gmail.com>
2023-06-09 22:16:08 +0300

    lession_3

--- todo_notes/todo_notes/urls.py
+++ todo_notes/todo_notes/urls.py
@@ -17,9 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
+from notes.views import NoteModelViewSet, ProjectModelViewSet
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from notes.views import NoteModelViewSet, ProjectModelViewSet
+from users.views import UserModelViewSet
 
 router = DefaultRouter()  # Create a router
 router.register('users', UserModelViewSet)  # Register User model view set with the router
+router.register('notes', NoteModelViewSet)  # Register Note model view set with the router
+router.register('projects', ProjectModelViewSet)  # Register Project model view set with the router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
