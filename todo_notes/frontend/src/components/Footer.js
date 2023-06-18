4bd0d372f12ed610d5350f6cdce83ee11e4a5fc1
Aleksandr <8712060@gmail.com>
2023-06-18 23:04:01 +0300

    lession_7

--- todo_notes/frontend/src/components/Footer.js
+++ todo_notes/frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Footer = () => {
     return (
         <footer className='footer-copyright fixed-bottom text-center bg-light py-3'>
-            &copy; {new Date().getFullYear()} Copyright: <a className='text-decoration-none' href='https://github.com/alexkifor'> Aleksandr Nikiforov </a>
+            &copy; {new Date().getFullYear()} Copyright: <a className='text-decoration-none' href='https://github.com/alexkifor/'> Aleksandr Nikiforov </a>
         </footer>
     )
 }
--- todo_notes/frontend/src/components/Menu.js
+++ todo_notes/frontend/src/components/Menu.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 
-const Menu = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to='/' className="ms-2 navbar-brand mb-0 h1">
-                Menu
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link to='/users' className="nav-item nav-link active">
-                        Users
-                    </Link>
-                    <Link to='/projects' className="nav-item nav-link active">
-                        Projects
-                    </Link>
-                    <Link to='/notes' className="nav-item nav-link active">
-                        Notes
-                    </Link>
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+                <Link to='/' className='ms-2 navbar-brand mb-0 h1'>
+                    Menu
+                </Link>
+                <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+                    <div className='navbar-nav'>
+                        <Link to='/users' className='nav-item nav-link active'>
+                            Users
+                        </Link>
+                        <Link to='/projects' className='nav-item nav-link active'>
+                            Projects
+                        </Link>
+                        <Link to='/notes' className='nav-item nav-link active'>
+                            Notes
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+                {this.props.isAuthenticated ?
+                    <a onClick={() => {
+                        this.props.logout()
+                    }} className='ms-2 navbar-brand'>{`${this.props.username} | Logout`}</a> :
+                    <Link to='/login' className='ms-2 navbar-brand'>
+                        Login
+                    </Link>
+                }
+            </nav>
+        )
+    }
 }
 
-export default Menu
+export default Menu;
