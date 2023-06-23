9f0cc89193f9f619f13633e9cdd567ee4aa0ebcf
Aleksandr <8712060@gmail.com>
2023-06-24 01:42:39 +0300

    lession_11

--- todo_notes/frontend/src/App.js
+++ todo_notes/frontend/src/App.js
@@ -10,8 +10,10 @@ import NoteList from './components/Note';
 import ProjectNotesList from './components/ProjectNote';
 import LoginForm from './components/Auth';
 import Cookies from 'universal-cookie/es6';
+import ProjectForm from "./components/ProjectForm";
+import NoteForm from "./components/NoteForm";
 
-const baseUrl = 'http://localhost:8000';
+const baseUrl = 'http://127.0.0.1:8000';
 const apiUrl = `${baseUrl}/api`;
 const getUrl = (path) => `${baseUrl}/${path}/`;
 const getApiUrl = (path) => `${apiUrl}/${path}/`;
@@ -107,6 +109,106 @@ class App extends React.Component {
         this.getTokenAndUserFromStorage();
     }
 
+    deleteNote(id) {
+        if (!this.isAuthenticated()) return;
+        const headers = this.getHeaders();
+        axios.delete(getApiUrl(`notes/${id}`), {headers})
+            .then(response => {
+                this.setState({
+                    notes: this.state.notes.map((note) => {
+                        if (note.id === id) {
+                            note.isActive = false;
+                        }
+                        return note;
+                    })
+                })
+                // Мы же не удаляем заметки полностью, а просто деактивируем. Поэтому надо просто снять галочку
+                // this.setState({notes: this.state.notes.filter((note) => note.id !== id)})
+            })
+            .catch(error => console.error(error));
+    }
+
+    restoreNote(id) {
+        const headers = this.getHeaders();
+        axios.patch(getApiUrl(`notes/${id}`), {isActive: true}, {headers})
+            .then(response => {
+                this.setState({
+                    notes: this.state.notes.map((note) => {
+                        if (note.id === id) {
+                            note.isActive = true;
+                        }
+                        return note;
+                    })
+                })
+            })
+            .catch(error => console.error(error));
+    }
+
+    updateNote(id) {
+        this.state.notes.find((note) => {
+            let condition = note.id === id;
+            if (condition) {
+                if (note.isActive) {
+                    this.deleteNote(id);
+                } else {
+                    this.restoreNote(id);
+                }
+            }
+            return condition;
+        });
+    }
+
+    createNote(props) {
+        if (!this.isAuthenticated()) return;
+        const headers = this.getHeaders();
+        props.isActive = true;
+        props.author = this.state.users.find((user) => user.username === this.state.username).id;
+        axios.post(getApiUrl(`notes`), {...props}, {headers})
+            .then(response => {
+                this.setState({notes: [...this.state.notes, response.data]})
+            })
+            .catch(error => console.log(error))
+    }
+
+    deleteProject(id) {
+        if (!this.isAuthenticated()) return;
+        const headers = this.getHeaders();
+        axios.delete(getApiUrl(`projects/${id}`), {headers})
+            .then(response => {
+                this.setState({projects: this.state.projects.filter((project) => project.id !== id)})
+            })
+            .catch(error => console.error(error));
+    }
+
+    createProject(props) {
+        if (!this.isAuthenticated()) return;
+        const headers = this.getHeaders();
+        props.users = [props.users,];
+        axios.post(getApiUrl(`projects`), {...props}, {headers})
+            .then(response => {
+                this.setState({projects: [...this.state.projects, response.data]})
+            })
+            .catch(error => console.log(error))
+    }
+
+    updateProject(props) {
+        if (!this.isAuthenticated()) return;
+        const headers = this.getHeaders();
+        axios.patch(getApiUrl(`projects/${props.id}`), {...props}, {headers})
+            .then(response => {
+                this.setState({
+                    projects: this.state.projects.map((project) => {
+                        if (project.id === props.id) {
+                            project = {...props};
+                        }
+                        return project;
+                    })
+                })
+            })
+            .catch(error => console.error(error));
+    }
+
+
     render() {
         return (
             <Router>
@@ -114,26 +216,39 @@ class App extends React.Component {
                       logout={() => this.logout()}
                       username={this.state.username}/>
                 <Switch>
-                <Route exact path='/users' component={() => {
+                    <Route exact path='/users' component={() => {
                         if (!this.isAuthenticated()) return <Redirect to='/login'/>;
                         return <UserList users={this.state.users}/>;
                     }}/>
                     <Route exact path='/projects' component={() => {
                         if (!this.isAuthenticated()) return <Redirect to='/login'/>;
                         return <ProjectList users={this.state.users}
-                                            projects={this.state.projects}/>;
+                                            projects={this.state.projects}
+                                            deleteProject={(id) => this.deleteProject(id)}/>;
+                    }}/>
+                    <Route exact path='/projects/create' component={() => {
+                        return <ProjectForm users={this.state.users}
+                                            createProject={(props) => this.createProject(props)}/>
                     }}/>
                     <Route path='/projects/:projectId' component={() => {
                         if (!this.isAuthenticated()) return <Redirect to='/login'/>;
                         return <ProjectNotesList users={this.state.users}
                                                  projects={this.state.projects}
-                                                 notes={this.state.notes}/>
+                                                 notes={this.state.notes}
+                                                 deleteProject={(id) => this.deleteProject(id)}
+                                                 updateProject={(props) => this.updateProject(props)}
+                                                 updateNote={(id) => this.updateNote(id)}/>
                     }}/>
                     <Route exact path='/notes' component={() => {
                         if (!this.isAuthenticated()) return <Redirect to='/login'/>;
                         return <NoteList users={this.state.users}
                                          notes={this.state.notes}
-                                         projects={this.state.projects}/>;
+                                         projects={this.state.projects}
+                                         updateNote={(id) => this.updateNote(id)}/>;
+                    }}/>
+                    <Route exact path='/notes/create' component={() => {
+                        return <NoteForm projects={this.state.projects}
+                                         createNote={(props) => this.createNote(props)}/>
                     }}/>
                     <Route exact path='/login' component={() => {
                         if (this.isAuthenticated()) return <Redirect to='/'/>;
